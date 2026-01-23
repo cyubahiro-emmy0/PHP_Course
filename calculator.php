5cef4c35c388918d94d243673d5df58b87b8d938
Emmy <cyubahiroemmy12@gmail.com>
2026-01-23 10:45:04 +0200

    Updated

--- calculator.php
+++ calculator.php
@@ -2,20 +2,39 @@
 <html>
 <head>
     <title>Simple Calculator</title>
+    <style>
+        .form-input {
+            width: 200px;
+            padding: 10px;
+            margin: 5px 0;
+            border: 1px solid #3b4dd8;
+            border-radius: 4px;
+            box-sizing: border-box; /* Ensures padding/border are included in the width */
+            font-size: 16px;
+        }
+
+        label{
+            font-size:20px;
+        }
+        input [type="submit"]{
+            background-color:green;
+        }
+
+    </style>
 </head>
 <body>
 
 <h2>Simple Calculator Operations</h2>
 
 <form method="post">
-    First Number:
-    <input type="number" name="num1" value="<?php echo $_POST['num1'] ?? ''; ?>" required><br><br>
+    <label for="num1">First Number:</label>
+    <input type="number" name="num1" class = "form-input" value="<?php echo $_POST['num1'] ?? ''; ?>" required><br><br>
 
-    Second Number:
-    <input type="number" name="num2" value="<?php echo $_POST['num2'] ?? ''; ?>" required><br><br>
+    <label for="num2">Second Number:</label>
+    <input type="number" name="num2" class ="form-input"  value="<?php echo $_POST['num2'] ?? ''; ?>" required><br><br>
 
-    Result:
-    <input type="text" name="result" 
+    <label for="result">Result:</label>
+    <input type="text" name="result" class="form-input"
            value="<?php echo $_POST['result'] ?? ''; ?>" readonly><br><br>
 
     <input type="submit" name="operator" value="+">
