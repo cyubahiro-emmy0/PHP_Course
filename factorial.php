4acf2f12fda4b4da4900c1f705684bbdb276c601
Emmy <cyubahiroemmy12@gmail.com>
2026-01-23 11:06:20 +0200

    Updated

--- factorial.php
+++ factorial.php
@@ -1,12 +1,28 @@
+<!DOCTYPE html>
 <html>
-    <?php
-    $number = 4;
+<head>
+    <title>Factorial Calculator</title>
+</head>
+<body>
+
+<form method="post">
+    <label>Enter a number:</label>
+    <input type="number" step="any"  name="number" min="0" required>
+    <button type="submit">Calculate</button>
+</form>
+
+<?php
+if (isset($_POST['number'])) {
+    $number = $_POST['number'];
     $factorial = 1;
 
-    for($i = 1; $i <= $number;$i++){
+    for ($i = 1; $i <= $number; $i++) {
         $factorial *= $i;
     }
 
-    echo "Factorial of $number is: $factorial\n"
-     ?>
+    echo "<p>Factorial of $number is: <strong>$factorial</strong></p>";
+}
+?>
+
+</body>
 </html>
